d3775d9c876132b71a932cbcdf80271b7970d2d3
erickzl <erick.vital@escola.pr.gov.br>
2024-09-11 23:00:15 +0000

    zl

--- sketch.js
+++ sketch.js
@@ -1,1 +1,13 @@
+function setup() {
+      createCanvas(400, 400);
+        background("orange");
+        }
 
+        function draw() {
+          stroke("red");
+            fill("gray");
+              
+                   if(mouseIsPressed) {
+                        rect(mouseX, mouseY, 20, 20);
+                          }
+                          }
